cdc56fb3a17db2b6edb7d2445b3330b04d3b3dba
Charlie Nader <nitro.naj@gmail.com>
2023-09-23 17:01:07 +1000

    adding reocil package

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,14 @@
 
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
+    <RecoilRoot>
+      <CacheProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </CacheProvider>
+    </RecoilRoot>
   );
 }
